b8702a4c863c83e1087511d7c55d0e0ee193dd2b
Kwang Su Choi <ksuchoi1219@gmail.com>
2017-08-22 14:13:08 -0400

    added

--- app/src/main/java/com/tresmore/treswallet/Dashboard.java
+++ app/src/main/java/com/tresmore/treswallet/Dashboard.java
@@ -5,15 +5,25 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -21,15 +31,15 @@ import java.sql.Statement;
 
 public class Dashboard extends AppCompatActivity {
 
-    public static DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
-
-    private Toolbar mToolbar;
-    private Button submitReceiptButton;
-    private Button trespassButton;
-    private TextView userCommissionTV;
-    private TextView userMemberCommissionTV;
-
+    private static final int BLACK = 0xFFFFFFFF;
+    private static final int WHITE = 0xFF000000;
+    private final static int WIDTH=500;
+    private TextView userCommission;
+    private TextView userTPCredit;
+    private TextView userCashbackCredit;
+    private TextView userTrespass;
+    private TextView userTotalCommission;
+    private ImageView userQRcode;
     //private ConnectionClass connectionClass;
     private String username = "";
 
@@ -37,37 +47,45 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
 
-        mToolbar = (Toolbar) findViewById(R.id.action_bar);
-//        userCommissionTV = (TextView) findViewById(R.id.userCommission);
 
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-//        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
-//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close) {
-//            @Override
-//            public void onDrawerOpened(View drawerView) {
-//                super.onDrawerOpened(drawerView);
-//            }
-//            @Override
-//            public void onDrawerClosed(View drawerView) {
-//                super.onDrawerClosed(drawerView);
-//            }
-//        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        mDrawerLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                mDrawerToggle.syncState();
-            }
-        });
-//        navigation();
-        addListenerOnButton();
+        userQRcode = (ImageView) findViewById(R.id.userQRcode);
+        userCommission = (TextView) findViewById(R.id.userCommission);
+        userTPCredit = (TextView) findViewById(R.id.userTPCredit);
+        userCashbackCredit = (TextView) findViewById(R.id.userCashbackCredit);
+        userTrespass = (TextView) findViewById(R.id.userTrespass);
+        userTotalCommission = (TextView) findViewById(R.id.userTotalCommission);
+
+        String STRuserCommission = userCommission.getText().toString();
+        float FuserComission = Float.valueOf(STRuserCommission);
+        String STRuserTPCredit = userTPCredit.getText().toString();
+        float FuserTPCredit = Float.valueOf(STRuserTPCredit);
+        String STRuserCashbackCredit = userCashbackCredit.getText().toString();
+        float FuserCashbackCredit = Float.valueOf(STRuserCashbackCredit);
+        String STRuserTrespass = userTrespass.getText().toString();
+        float FuserTrespass = Float.valueOf(STRuserTrespass);
+
+        float totalCommission = FuserCashbackCredit + FuserComission + FuserTPCredit + FuserTrespass;
+        String STRtotalCommission = String.valueOf(totalCommission);
+        userTotalCommission.setText(STRtotalCommission);
+
+        SharedPreferences prefs = getSharedPreferences("MA", MODE_PRIVATE);
+        username = prefs.getString("UN", "UNKNOWN");
+        String userQRinfo = username + " " + STRtotalCommission;
+
+        try {
+            Bitmap bitmap = encodeAsBitmap(userQRinfo);
+            userQRcode.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+
 
 //        connectionClass = new ConnectionClass();
-//        SharedPreferences prefs = getSharedPreferences("MA", MODE_PRIVATE);
-//        username = prefs.getString("UN", "UNKNOWN");
+
 //        Connection con = connectionClass.CONN();
 //        String query = "select commission from dbo.users where loginid='" + username + "';";
 //        ResultSet rs;
@@ -86,47 +104,29 @@ public class Dashboard extends AppCompatActivity {
 //            ex.getMessage();
 //        }
     }
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        Bitmap bitmap = null;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, WIDTH, WIDTH, null);
 
-    private void addListenerOnButton() {
-        final Context context = this;
-        submitReceiptButton = (Button) findViewById(R.id.submitReceipteButton);
-        submitReceiptButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog alertDialog = new AlertDialog.Builder(Dashboard.this).create();
-                alertDialog.setTitle("Feature");
-                alertDialog.setMessage("Coming Soon!");
-                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        });
-                alertDialog.show();
+            int w = result.getWidth();
+            int h = result.getHeight();
+            int[] pixels = new int[w * h];
+            for (int y = 0; y < h; y++) {
+                int offset = y * w;
+                for (int x = 0; x < w; x++) {
+                    pixels[offset + x] = result.get(x, y) ? WHITE : BLACK;
+                }
             }
-        });
-//        submitReceiptButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(context, Receipt.class);
-//                startActivity(intent);
-//            }
-//        });
-        trespassButton = (Button) findViewById(R.id.trespassButton);
-        trespassButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog alertDialog = new AlertDialog.Builder(Dashboard.this).create();
-                alertDialog.setTitle("Feature");
-                alertDialog.setMessage("Coming Soon!");
-                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        });
-                alertDialog.show();
-            }
-        });
+            bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, w, 0, 0, w, h);
+        } catch (Exception iae) {
+            iae.printStackTrace();
+            return null;
+        }
+        return bitmap;
     }
+
 }
